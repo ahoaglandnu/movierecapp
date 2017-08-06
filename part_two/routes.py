c074eb251db351e39e44a08265054e60ce3b1a10
Andy Hoagland <ahoaglandnu@users.noreply.github.com>
2017-08-06 14:17:01 -0400

    Add files via upload

--- part_two/routes.py
+++ part_two/routes.py
@@ -15,6 +15,7 @@ def films():
 	c = conn.cursor()
 	c.execute("select movies, count(*) from reviews group by movies order by count(*) desc;")
 	rows = c.fetchall();
+	c.close()
 	return render_template('films.html', rows=rows)
 
 # Single Movie
@@ -24,13 +25,40 @@ def film(movies):
 	c = conn.cursor()
 	c.execute("select critic from reviews where liked=1 and movies = ? ;", [movies])
 	likes = c.fetchall();
-	c.execute("select critic from reviews where liked=0 and movies= ? ;", [movies])
+	c.execute("select critic from reviews where liked=0 and movies = ? ;", [movies])
 	dislikes = c.fetchall();
 	c.execute("select count(*) from reviews where liked=1 and movies = ? ;", [movies])
 	numLikes = c.fetchone();
-	c.execute("select count(*) from reviews where liked=0 and movies= ? ;", [movies])
+	c.execute("select count(*) from reviews where liked=0 and movies = ? ;", [movies])
 	numDislikes = c.fetchone();
+	c.close()
 	return render_template('film.html', likes=likes, dislikes=dislikes, movies=movies, numDislikes=numDislikes, numLikes=numLikes)
+
+# Critics
+@app.route('/critics')
+def critics():
+	conn = sql.connect('movie_db.db')
+	c = conn.cursor()
+	c.execute("select critic, count(*) from reviews group by critic order by count(*) desc;")
+	rows = c.fetchall()
+	c.close()
+	return render_template('critics.html', rows=rows)
+
+# Single Critic
+@app.route('/critic/<string:critic>')
+def review(critic):
+	conn = sql.connect('movie_db.db')
+	c = conn.cursor()
+	c.execute("select movies from reviews where liked=1 and critic = ? ;", [critic])
+	likes = c.fetchall();
+	c.execute("select movies from reviews where liked=0 and critic = ? ;", [critic])
+	dislikes = c.fetchall();
+	c.execute("select count(*) from reviews where liked=1 and critic = ? ;", [critic])
+	numLikes = c.fetchone();
+	c.execute("select count(*) from reviews where liked=0 and critic = ? ;", [critic])
+	numDislikes = c.fetchone();
+	c.close()
+	return render_template('critic.html', likes=likes, dislikes=dislikes, critic=critic, numDislikes=numDislikes, numLikes=numLikes)
 
 
 if __name__ == '__main__':
